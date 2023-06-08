9e3382f35ebf32f33779a4d0d2adc7c1a2d00750
sravettini <sravettini@udesa.edu.ar>
2023-06-08 15:44:34 -0300

    generos

---
+++ js/genres.js
@@ -0,0 +1,29 @@
+let urlGeneros= "https://cors-anywhere.herokuapp.com/https://api.deezer.com/genre"
+
+fetch (urlGeneros)
+.then(function(response){
+    return response.json()
+})
+.then(function(data){
+    console.log(data);
+
+    let generos= document.querySelector (".listado-generos");
+    let arrayGeneros= data.data
+    let generosHTML= ""
+    
+    for(let i=0;i<arrayGeneros.length;i++){
+        generosHTML += `<article class= "article-generos">
+        <a href:"detail-genres.html?id=${arrayGeneros[i].id}">
+        <h2>${arrayGeneros[i].title}</h2>
+        <img src= ${arrayGeneros[i].picture_medium}>
+        </a>
+        </article>`
+    }
+    generos.innerHTML=generosHTML
+})
+
+.catch(function(error){
+    console.log("Error:"+ error);
+})
+
+
