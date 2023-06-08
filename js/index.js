7d6e4542f15023fa96466b82cb0366af92b09cab
Markelesperanza <mesperanza@udesa.edu.ar>
2023-06-08 01:45:54 -0300

    Creacion del js del punto 2

--- js/index.js
+++ js/index.js
@@ -1,19 +1,87 @@
 /***** HOME *****/
-let urlCanciones = ""
+let urlCanciones= "https://cors-anywhere.herokuapp.com/https://api.deezer.com/chart/0/tracks"
 
 fetch(urlCanciones)
-.then(function(response) {
-  return response.json()
+.then(function(response){
+    return response.json()
 })
-.then(function(data) {
-  console.log(data);
+.then(function(data){
+    console.log(data);
+
+    let canciones= document.querySelector(".listado-canciones");
+    let arrayCanciones= data.data
+    let cancionesHTML= ''
+
+    for(let i=0; i<arrayCanciones.length; i++){
+        cancionesHTML += `<article class="article-canciones">
+        <a class="" href="https://api.deezer.com/chart/0/tracks${arrayCanciones[i].id}">
+        <img src= ${arrayCanciones[i].album.cover_medium}>
+        <h2>${arrayCanciones[i].title}</h2>
+        <h3>${arrayCanciones[i].artist.name}</h3>
+        </a>
+        </article>`
+
+    }
+    canciones.innerHTML= cancionesHTML;
+
+})
+.catch(function(error){
+    console.log("Error: " + error);
+})
+
+let urlAlbum= "https://cors-anywhere.herokuapp.com/https://api.deezer.com/chart/0/albums"
+
+fetch(urlAlbum)
+.then(function(response){
+    return response.json()
+})
+.then(function(data){
+    console.log(data);
+
+    let album= document.querySelector(".listado-albumes");
+    let arrayAlbum= data.data
+    let albumHTML= ''
+
+    for(let i=0; i<arrayAlbum.length; i++){
+        albumHTML += `<article class="article-album">
+        <img src= ${arrayAlbum[i].cover_medium}>
+        <h2>${arrayAlbum[i].title} </h2>
+        </article>`
+
+    }
+    album.innerHTML= albumHTML;
+})
+.catch(function(error){
+    console.log("Error: " + error);
+})
 
 
-})
-.catch(function(error) {
-  console.log("Error: " + error);
-})
+let urlArtistas= "https://cors-anywhere.herokuapp.com/https://api.deezer.com/chart/0/artists"
 
+fetch(urlArtistas)
+.then(function(response){
+    return response.json()
+})
+.then(function(data){
+    console.log(data);
+
+    let artistas= document.querySelector(".listado-artistas");
+    let arrayArtistas= data.data
+    let artistasHTML= ''
+
+    for(let i=0; i<arrayArtistas.length; i++){
+        artistasHTML += `<article class="article-artistas">
+        <a class="" href="https://api.deezer.com/chart/0/tracks${arrayCanciones[i].id}">
+        <img src= ${arrayArtistas[i].picture_medium}>
+        <h2>${arrayArtistas[i].name} </h2>
+        </a>
+        </article>`
+
+    }
+    artistas.innerHTML= artistasHTML;
+
+})
+.catch(function(error){
+    console.log("Error: " + error);
+})
 /***** HOME *****/
-
-
