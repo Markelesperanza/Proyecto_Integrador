52bd15060c4cf75ac03db3c9a329e7d112c2e0a4
Markelesperanza <mesperanza@udesa.edu.ar>
2023-06-15 19:01:07 -0300

    modificacion del punto 4

--- js/playlist.js
+++ js/playlist.js
@@ -1,25 +1,31 @@
 let recuperoStorage = localStorage.getItem("MiPlaylist");
 let cancionesRecuperadas = JSON.parse(recuperoStorage);
 
-let imagenPlaylist = document.querySelector('.playlist-favoritos');
+let imgplaylist = document.querySelector(".canciones-de-la-playlist");
 
-let tituloPlaylist = document.querySelector('.tituloPlaylist');
+let tituloplaylist = document.querySelector(".playlist-titulo");
 
 if(cancionesRecuperadas.length == 0){
-    tituloPlaylist.innerHTML = 'No agregaste ninguna cacion a tu playlist'
-} else{
+  tituloplaylist.innerHTML = "Tu Playlist esta vacía"
 
-    for(let i=0 ; i < cancionesRecuperadas.length; i++){
+}else{
+  for(let i=0 ; i < cancionesRecuperadas.length; i++){
+    fetch(`https://cors-anywhere.herokuapp.com/https://api.deezer.com/track/${cancionesRecuperadas[i]}`)
+    
+    .then(function(response){
+    return response.json()
 
-        let urlRecuperados = `https://cors-anywhere.herokuapp.com/https://api.deezer.com/track/${cancionesRecuperadas[i]}`
+    })
+    .then(function(track){
+      imgplaylist.innerHTML += `<a href="detalles-canciones.html?id=${track.id}">
+      <img src="${track.album.cover}">
+      <h3>${track.title}</h3>
+      </a>`
+    
 
-        fetch(urlRecuperados)
-        .then(function(response){
-            return response.json()
-        })
-        .then(function(track){
-            imagenPlaylist.innerHTML +=  ` <a href="detalles-canciones.html?id=${track.id}"><img src=${track.album.cover_medium}`
-        })
-        .catch(function(error){
-            console.log(error);
-        })}}
+    })
+    .catch(function(error){
+      console.log("Error: " + error);
+    })
+  }
+}
