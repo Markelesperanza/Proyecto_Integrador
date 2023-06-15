4b3187cfd78f9c5c3aa11e3b31d0ce61a69a893d
JoacoRosas <jrosaszuiani@udesa.edu.ar>
2023-06-15 13:54:13 -0300

    todo el punto 4

--- js/playlist.js
+++ js/playlist.js
@@ -1,11 +1,24 @@
-let cancionesPlaylist = document.querySelector(".canciones-playlist");
-
 let recuperoStorage = localStorage.getItem("MiPlaylist");
 let cancionesRecuperadas = JSON.parse(recuperoStorage);
 
-let urlLocalStorage = `https://cors-anywhere.herokuapp.com/https://api.deezer.com/track/${recuperoStorage}`
+let imagenPlaylist = document.querySelector('.playlist-favoritos');
 
-let localStorageHTML = ""
-for(let i=0; i<cancionesRecuperadas.length; i++){
-    localStorageHTML += `<li>${cancionesRecuperadas[i].recuperoStorage}</li>`
-}
+let tituloPlaylist = document.querySelector('.tituloPlaylist');
+
+if(cancionesRecuperadas.length == 0){
+    tituloPlaylist.innerHTML = 'No agregaste ninguna cacion a tu playlist'
+} else{
+
+    let urlRecuperados = `https://cors-anywhere.herokuapp.com/https://api.deezer.com/track/${cancionesRecuperadas[i]}`
+
+    for(let i=0 ; i < cancionesRecuperadas.length; i++){
+        fetch(urlRecuperados)
+        .then(function(response){
+            return response.json()
+        })
+        .then(function(track){
+            imagenPlaylist.innerHTML +=  ` <a href="detalles-canciones.html?id=${track.id}"><img src=${track.album.cover_medium}`
+        })
+        .catch(function(error){
+            console.log(error);
+        })}}
